ad126328fc730672c584dedf13b2cfdd1b4020d4
ProSavage <naman0311@gmail.com>
2019-03-24 09:04:25 -0500

    colors & layout

--- imports/ui/Column.js
+++ imports/ui/Column.js
@@ -4,6 +4,12 @@ import {Users} from "../../lib/Users";
 
 
 class Column extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { color: 'rgba(112, 147, 212, 0.2)' }
+    }
+
+
     removeTask(i) {
         let user = Users.findOne({_id: this.props.id});
         let tasks = user.tasks;
@@ -31,7 +37,9 @@ class Column extends React.Component {
 
                 <input onClick={this.changeTaskStatus.bind(this, i)} className={"small-padding small-margin checkmark"} checked={this.props.tasks[i].status} id={'checkbox-' + this.props.id} type={'checkbox'}/>
                 <span className={'checkmark'}/>
+                <div>
                 {this.props.tasks[i].task}
+                </div>
                 <button onClick={this.removeTask.bind(this, i)} className={"small-padding small-margin x-button"}>x</button>
 
             </label>
@@ -53,8 +61,18 @@ class Column extends React.Component {
     }
 
     generateRGB() {
-        return 'rgba(' + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ", 0.2)";
-    }
+        console.log('Colors...');
+        if (this.calculatePoints() / (this.props.tasks.length * 10) === 1) {
+            console.log("green");
+            return 'rgba(112,255,100,0.2)';
+        } else if (this.calculatePoints() === 0) {
+            console.log('red');
+            return 'rgba(255,100,124,0.2)'
+        } else {
+            console.log('blue');
+            return ;
+        }
+    };
 
     deleteColumn() {
         Users.remove({_id: this.props.id});
@@ -62,7 +80,7 @@ class Column extends React.Component {
 
     render() {
         return (
-                <div style={{backgroundColor: 'rgba(112,147,212,0.2)'}} className={"column"}>
+                <div style={{backgroundColor: this.generateRGB()}} className={"column"}>
                     <div className={"card-wrapper"}>
                         <div className={"wrapper"}>
                             <div className={"card"}>
@@ -70,22 +88,31 @@ class Column extends React.Component {
                                     <AddTask id={this.props.id}/>
                                 </div>
                                 <div>
-                                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                                    <h2>{this.props.name}</h2>
-                                    <button onClick={this.deleteColumn.bind(this)} className={'x-button'}>X</button>
+                                    <div style={{display: 'flex',flexWrap: 'wrap', justifyContent: 'space-between',width: '100%'}}>
+                                        <div style={{display: 'flex',flexWrap: 'wrap', justifyContent: 'space-between',width: '100%'}}>
+                                        <h2>{this.props.name}</h2>
+                                        <button onClick={this.deleteColumn.bind(this)} className={'x-button'}>X</button>
+                                        </div>
+                                        <br/>
+                                        <div style={{width: '100%'}}>
+                                            <h4>Tasks</h4>
+                                        </div>
+
+                                        <br/>
+                                        <div>{this.renderTasks()}</div>
+
                                     </div>
-                                    <br/>
-                                    <h4>Tasks</h4>
-                                    <br/>
+
                                 </div>
-                                {this.renderTasks()}
+
                                 <br/>
-                                    <h1 style={{    position: "absolute", bottom: "15px"}}>{this.calculatePoints()} Points</h1>
+                                <h1 style={{position: "absolute", bottom: "15px"}}>{this.calculatePoints() }/{ (this.props.tasks.length * 10)}  Points</h1>
                             </div>
 
                         </div>
                     </div>
-            </div>
+                </div>
+
         )
     }
 }
