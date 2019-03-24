3b2e51384441b19bca186efd9e8685d1acdfd2dd
ProSavage <naman0311@gmail.com>
2019-03-24 06:55:37 -0500

    global tasks

--- imports/ui/AddUser.js
+++ imports/ui/AddUser.js
@@ -30,14 +30,33 @@ class AddUser extends React.Component {
         });
         alert("Winner " + winner.name);
     }
+
+    addGroupTask() {
+        let taskName = document.getElementById('name-of-user').value;
+        if (taskName) {
+            let users = Users.find().fetch();
+            users.forEach((user) => {
+                let tasks = user.tasks;
+                if (user.tasks.length === undefined) {
+                    tasks = [];
+                }
+                tasks[tasks.length] = {task: taskName, status: false};
+                user.tasks = tasks;
+                Users.update({_id: user._id}, user);
+            })
+        }
+
+    }
+
     render() {
         return (
             <React.Fragment>
                 <form>
-                    <input className={"column-button input-shadow"} placeholder={"  Add a User..."} id={'name-of-user'}/>
+                    <input className={"column-button input-shadow"} placeholder={"  Add a User or Group Task"} id={'name-of-user'}/>
                     <button className={"column-button input-button"} onClick={this.addUser.bind(this)}>Add User</button>
                 </form>
-                <button className={'column-button leaderboard-button'} onClick={this.getLeader}>Leaderboard</button>
+                <button className={'column-button input-button'} onClick={this.getLeader}>Leaderboard</button>
+                <button className={'column-button input-button'} onClick={this.addGroupTask}>Add Group Task</button>
             </React.Fragment>
 
         );
